cb034b062047e1df75dbb7ed12b1874a20021ada
Vladislav <wlladisla@gmail.com>
2022-09-16 23:27:48 +0300

    feat:aad2

---
+++ color_brightest.js
@@ -0,0 +1,21 @@
+/* You are given a list of colors in 6-digit hexidecimal notation #RRGGBB. Return the brightest of these colors!*/
+
+// function brightest(colors) {
+//   debugger;
+// }
+
+// let arr = ['ABCDEF', '123456'];
+// // console.log(arguments[2]);
+// // return arguments.map(i => i+100)
+
+// function ff() {
+//   let res = 0;
+//   for (let i = 0; i < arguments.length; i++) {
+//     res+= arguments[i];
+
+//   }
+//   return res;
+// }
+
+// console.log( ff(2,3,4,5,7));
+
