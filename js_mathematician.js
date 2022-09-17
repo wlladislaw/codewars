ce1a827d29ad1997a4f5a8eab83dadec98610950
Vladislav <wlladisla@gmail.com>
2022-09-17 23:11:32 +0300

    fix:solve

--- js_mathematician.js
+++ js_mathematician.js
@@ -1,11 +1,16 @@
-function calculate(func) {
-  return function curried(...args) {
-    if (args.length >= func.length) {
-      return func.apply(this, args);
-    } else {
-      return function(...args2) {
-        return curried.apply(this, args.concat(args2));
-      }
+
+
+function calculate() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return function () {
+    for (let i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
     }
+    return sum;
   };
 }
+
+// console.log(calculate(143, 13)(1100)); // should return 3
